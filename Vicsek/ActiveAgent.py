0fa44e86e16f63e0ed13abfd74a45928eb225e65
Torrki <kioer.exe@gmail.com>
2024-05-07 01:24:16 +0200

    Simulazione Vicsek completata

---
+++ Vicsek/ActiveAgent.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class ActiveAgent:
+	'''
+	Classe per rappresentare gli agenti attivi del modello Vicsek
+	r 		è la posizione, un vettore colonna 2x1
+	s 		è l'orientamento, un vettore colonna 2x1
+	lo stato è descritto dall'unione dei due vettori
+	 _____
+	|     |
+	|  r  |
+	|_____|
+	|     |
+	|  s  |
+	|_____|
+	'''
+	raggioIntorno=3
+	def __init__(self, r0, s0):
+		self.stato=np.concatenate((r0,s0), dtype=np.dtype(float))
+	
+	def getPosition(self):
+		return self.stato[[0,1]]
+		
+	def getDirection(self):
+		return self.stato[[2,3]]
+		
+	def setPosition(self, r):
+		self.stato[[0,1]]=r
+		
+	def setDirection(self, s):
+		self.stato[[2,3]]=s
+		
+	def inBound(self, a):
+		diff_vect=self.stato[[0,1]]-a.stato[[0,1]]
+		return np.linalg.norm(diff_vect) < ActiveAgent.raggioIntorno
---
+++ Vicsek/main.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib as plot
+import numpy.random as rnd
+from ActiveAgent import ActiveAgent
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+seme_random=55
+gen=rnd.default_rng(seme_random) #Random Generator
+
+def Vicsek(T: float, density: float, v0: float, N:int, eta: float): #funzione decoratore per configurare la simulazione
+	'''
+	Funzione per la configurazione di un modello Vicsek:
+	T 					è il periodo di osservazione
+	density 		è la densità degli agenti nello spazio
+	v0 					è la velocità degli agenti
+	N						è il numero di agenti attivi
+	eta					é l'intensità del rumore
+	
+	torna una funzione che permette di eseguire la simulazione
+	'''
+	L=N/(2.0*density) #Raddoppio perchè lo spazio va da -L a L, dunque in lunghezza 2L
+	
+	posizioniIniziali=gen.uniform(low=-L, high=L, size=(2,N))
+	angoliIniziali=gen.uniform(high=2*np.pi, size=N)
+	orientamentiIniziali=np.array( [[np.cos(a) for a in angoliIniziali],
+																	[np.sin(a) for a in angoliIniziali]] )
+																	
+	agenti=[ActiveAgent(np.resize( posizioniIniziali[:,k], (2,1) ), np.resize( orientamentiIniziali[:,k], (2,1) )) for k in range(N)] #creazione degli N agenti con gli stati iniziali
+	
+	def sistema(passi: int):
+		'''
+		Funzione per la simulazione del modello Vicsek
+		passi			è il numero di passi della simulazione
+		
+		Torna una matrice (N*2)Xpassi in cui la colonna k-esima è lo stato del sistema al passo k-esimo
+		'''
+		statoVicsek=agenti[0].getPosition() #creo lo stato iniziale del sistema, che comprende solo le posizioni degli agenti
+		for i in range(1, N):
+			statoVicsek=np.concatenate((statoVicsek, agenti[i].getPosition()))
+		snaps=np.copy(statoVicsek)
+		
+		#simulazione dei passi
+		for k in range(passi):
+			connettivita=np.zeros((N,N))
+			for i in range(N):
+				i_stato=i*2
+				s_i=agenti[i].getDirection()
+				
+				s_vicini=np.zeros((2,1)) #vettore medio della direzione dei vicini, compreso anche l'agente i-esimo
+				for j in range(N):
+					connettivita[j,i]=agenti[i].inBound(agenti[j])
+					s_vicini += connettivita[j,i]*agenti[j].getDirection()
+					
+				rumore=gen.standard_normal(size=(2,1))
+				rumore_norm = rumore/np.linalg.norm(rumore)
+					
+				statoVicsek[[i_stato,i_stato+1]] += T*v0*s_i #aggiornamento delle posizioni nello stato
+				agenti[i].setPosition(statoVicsek[[i_stato,i_stato+1]])
+				
+				new_s= (s_vicini+eta*rumore_norm)/np.linalg.norm(s_vicini+eta*rumore_norm) #aggiornamento dell'orientamento degli agenti
+					
+				agenti[i].setDirection( new_s )
+				
+			snaps=np.concatenate((snaps, statoVicsek),axis=1) #aggiungo il nuovo stato nella raccolta
+		return snaps
+	
+	return sistema
+	
+
+
+def main():
+	n_snaps=500
+	n_agenti=30
+	T=0.02
+	v0=4.0
+	linear_density=1
+	eta_random=0.4
+	
+	simulazione=Vicsek(T, linear_density, v0, n_agenti, eta_random)
+	istanti=simulazione(n_snaps)
+	
+	#Animazione
+	fig, ax=plt.subplots()
+	points, =ax.plot([], [],'o')
+	
+	def update_points(nframe):
+		x_plt=list()
+		y_plt=list()
+		for i in range(n_agenti):
+			x_plt.append( istanti[i*2,nframe] )
+			y_plt.append( istanti[i*2+1,nframe] )
+			
+		points.set_data( np.array([ x_plt,
+																y_plt ]) )
+		return points,
+	
+	L_plot=(n_agenti/(2.0*linear_density))+30
+	ax.set_xlim([-L_plot,L_plot])
+	ax.set_ylim([-L_plot,L_plot])
+	
+	anim=animation.FuncAnimation(fig, update_points, n_snaps, interval=T*1000.0, blit=True)
+	plt.show()
+	
+if __name__=="__main__":
+	main()
+	
